a595b1a7d7a05749cbb5d4a85c834f5ba973eea8
honk <github@honk.email>
2023-04-16 19:59:56 +0200

    misc: make the badge code a little easier to read

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -3,7 +3,7 @@ import React, { FC } from "react";
 interface BadgeProps {
   text: string;
   bgColor: string;
-  
+
   logo?: string;
   logoColor?: string;
 }
@@ -11,9 +11,11 @@ interface BadgeProps {
 const Badge: FC<BadgeProps> = ({ text, logo, bgColor, logoColor }) => {
   bgColor = bgColor.replace("#", "");
   logoColor = logoColor?.replace("#", "");
+
   if (logoColor && !logo) {
     throw new Error("You must provide a logo when using logoColor.");
   }
+
   let src = `https://img.shields.io/badge/${text}-${bgColor}?style=for-the-badge${logo ? `&logo=${logo}` : ""}${logoColor ? `&logoColor=${logoColor}` : ""}`;
   return <img src={src} alt={text} />;
 };
